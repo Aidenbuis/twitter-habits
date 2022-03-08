c162710e0a739f620adc6c22702f956f50ef4bed
Aiden Buis <hello@aidenbuis.com>
2022-03-08 23:05:57 +0100

    🐺 🐺

--- chatbot.ts
+++ chatbot.ts
@@ -5,6 +5,8 @@ import cron = require('cron')
 
 dotenv.config()
 
+console.log('TEST123')
+
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY || '',
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET || '',
